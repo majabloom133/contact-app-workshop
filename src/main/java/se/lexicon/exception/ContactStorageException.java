53e30d8144659e069fbb871056895e6c99ce8047
Hans Malmefjäll <hans.malmefjall@gmail.com>
2026-04-16 10:34:44 +0200

    Add custom exception files
    add ContactStorageException.java
    add DuplicateContactException.java

---
+++ src/main/java/se/lexicon/exception/ContactStorageException.java
@@ -0,0 +1,7 @@
+package se.lexicon.exception;
+
+public class ContactStorageException extends Exception {
+    public ContactStorageException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/se/lexicon/exception/DuplicateContactException.java
@@ -0,0 +1,7 @@
+package se.lexicon.exception;
+
+public class DuplicateContactException extends Exception {
+    public DuplicateContactException(String message) {
+        super(message);
+    }
+}
